c6600aae98304069cf06b49613dac24f13e5af38
Hiale.dev <Hiale.dev@c269392e-2877-11df-ab0a-9f3f633ae91d>
2013-06-03 21:45:00 +0000

    Map Collision WIP (still...)

--- trunk/GTA2.NET/GTA2.NET/GTA2.NET Core/Helper/Debug.cs
+++ trunk/GTA2.NET/GTA2.NET/GTA2.NET Core/Helper/Debug.cs
@@ -25,6 +25,8 @@
 // Grand Theft Auto (GTA) is a registred trademark of Rockstar Games.
 using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using Hiale.GTA2NET.Core.Collision;
@@ -47,7 +49,7 @@ namespace Hiale.GTA2NET.Core.Helper
                 for (var i = 0; i < 7; i++)
                 {
                     var obstacles = collision.GetObstacles(i);
-                    //DisplayCollision(obstacles);
+                    DisplayCollision(obstacles);
                 }
             }
             catch (Exception e)
@@ -55,6 +57,63 @@ namespace Hiale.GTA2NET.Core.Helper
                 System.Diagnostics.Debug.WriteLine(e.StackTrace);
             }
             
+        }
+
+        private static void DisplayCollision(IEnumerable<IObstacle> obstacles)
+        {
+            var layers = new Dictionary<int, Bitmap>();
+            foreach (var obstacle in obstacles)
+            {
+                Bitmap bmp;
+                if (layers.ContainsKey(obstacle.Z))
+                {
+                    bmp = layers[obstacle.Z];
+                }
+                else
+                {
+                    bmp = new Bitmap(2560, 2560);
+                    layers.Add(obstacle.Z, bmp);
+                }
+
+                using (var g = Graphics.FromImage(bmp))
+                {
+                    if (obstacle is RectangleObstacle)
+                    {
+                        var rectObstacle = (RectangleObstacle)obstacle;
+                        g.FillRectangle(new SolidBrush(System.Drawing.Color.Red), rectObstacle.Position.X * 10, rectObstacle.Position.Y * 10, rectObstacle.Width * 10, rectObstacle.Length * 10);
+                    }
+                    else if (obstacle is LineObstacle)
+                    {
+                        var lineObstacle = (LineObstacle)obstacle;
+                        g.DrawLine(new Pen(System.Drawing.Color.Magenta), new System.Drawing.Point((int)lineObstacle.Start.X * 10, (int)lineObstacle.Start.Y * 10), new System.Drawing.Point((int)lineObstacle.End.X * 10, (int)lineObstacle.End.Y * 10));
+                    }
+                    //else if (obstacle is FallEdge)
+                    //{
+                    //    var fallEdge = (FallEdge)obstacle;
+                    //    g.DrawLine(new Pen(System.Drawing.Color.Turquoise), new System.Drawing.Point((int)fallEdge.Start.X * 10, (int)fallEdge.Start.Y * 10), new System.Drawing.Point((int)fallEdge.End.X * 10, (int)fallEdge.End.Y * 10));
+                    //}
+                    //else if (obstacle is SlopeObstacle)
+                    //{
+                    //    var slopeObstacle = (SlopeObstacle)obstacle;
+                    //    g.FillRectangle(new SolidBrush(System.Drawing.Color.Blue), slopeObstacle.Position.X * 10, slopeObstacle.Position.Y * 10, 10, 10);
+                    //}
+                    else if (obstacle is PolygonObstacle)
+                    {
+                        var polygonObstacle = (PolygonObstacle)obstacle;
+                        var points = new System.Drawing.Point[polygonObstacle.Vertices.Count];
+                        for (var i = 0; i < polygonObstacle.Vertices.Count; i++)
+                            points[i] = new System.Drawing.Point((int)polygonObstacle.Vertices[i].X * 10, (int)polygonObstacle.Vertices[i].Y * 10);
+                        g.FillPolygon(new SolidBrush(System.Drawing.Color.OrangeRed), points);
+                    }
+
+                }
+            }
+
+            foreach (var pair in layers)
+            {
+                pair.Value.Save(pair.Key + ".png", ImageFormat.Png);
+                pair.Value.Dispose();
+            }
         }
     }
 }
