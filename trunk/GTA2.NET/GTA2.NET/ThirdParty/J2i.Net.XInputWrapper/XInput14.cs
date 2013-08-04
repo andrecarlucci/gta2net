14f754961e9622ce318a5d98698879f46ae11c05
hiale.dev <hiale.dev@c269392e-2877-11df-ab0a-9f3f633ae91d>
2013-08-04 15:59:11 +0000

    XInput Gamepad Fix

---
+++ trunk/GTA2.NET/GTA2.NET/ThirdParty/J2i.Net.XInputWrapper/XInput14.cs
@@ -0,0 +1,47 @@
+﻿
+using System.Runtime.InteropServices;
+
+namespace J2i.Net.XInputWrapper
+{
+    public static class XInput14
+    {
+        [DllImport("xinput1_4.dll")]
+        public static extern int XInputGetState
+        (
+            int dwUserIndex,  // [in] Index of the gamer associated with the device
+            ref XInputState pState        // [out] Receives the current state
+        );
+
+        [DllImport("xinput1_4.dll")]
+        public static extern int XInputSetState
+        (
+            int dwUserIndex,  // [in] Index of the gamer associated with the device
+            ref XInputVibration pVibration    // [in, out] The vibration information to send to the controller
+        );
+
+        [DllImport("xinput1_4.dll")]
+        public static extern int XInputGetCapabilities
+        (
+            int dwUserIndex,   // [in] Index of the gamer associated with the device
+            int dwFlags,       // [in] Input flags that identify the device type
+            ref XInputCapabilities pCapabilities  // [out] Receives the capabilities
+        );
+
+
+        [DllImport("xinput1_4.dll")]
+        public static extern int XInputGetBatteryInformation
+        (
+            int dwUserIndex,        // Index of the gamer associated with the device
+            byte devType,            // Which device on this user index
+            ref XInputBatteryInformation pBatteryInformation // Contains the level and types of batteries
+        );
+
+        [DllImport("xinput1_4.dll")]
+        public static extern int XInputGetKeystroke
+        (
+            int dwUserIndex,              // Index of the gamer associated with the device
+            int dwReserved,               // Reserved for future use
+            ref XInputKeystroke pKeystroke    // Pointer to an XINPUT_KEYSTROKE structure that receives an input event.
+        );
+    }
+}
