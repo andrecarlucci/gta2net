7cd478234978c655fa240040cbe30cb48d4e94d0
hiale.dev <hiale.dev@c269392e-2877-11df-ab0a-9f3f633ae91d>
2013-08-04 15:59:11 +0000

    XInput Gamepad Fix

--- GTA2.NET/GTA2.NET/ThirdParty/J2i.Net.XInputWrapper/XInput.cs
+++ GTA2.NET/GTA2.NET/ThirdParty/J2i.Net.XInputWrapper/XInput.cs
@@ -1,101 +1,73 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace J2i.Net.XInputWrapper
 {
     public static class XInput
     {
-#if WINDOWS7
-        [DllImport("xinput9_1_0.dll")]
-        public static extern int XInputGetState
-        (
-            int dwUserIndex,  // [in] Index of the gamer associated with the device
-            ref XInputState pState        // [out] Receives the current state
-        );
+        private static bool? _isWin8OrNewer;
 
-        [DllImport("xinput9_1_0.dll")]
-        public static extern int XInputSetState
-        (
-            int dwUserIndex,  // [in] Index of the gamer associated with the device
-            ref XInputVibration pVibration    // [in, out] The vibration information to send to the controller
-        );
+        private static bool IsWin8OrNewer
+        {
+            get
+            {
+                if (_isWin8OrNewer == null)
+                {
+                    var os = Environment.OSVersion;
+                    _isWin8OrNewer = os.Platform == PlatformID.Win32NT && (os.Version.Major > 6 || (os.Version.Major == 6 && os.Version.Minor >= 2));
+                }
+                return _isWin8OrNewer.Value;
+            }
+        }
 
-        [DllImport("xinput9_1_0.dll")]
-        public static extern int XInputGetCapabilities
-        (
-            int dwUserIndex,   // [in] Index of the gamer associated with the device
-            int dwFlags,       // [in] Input flags that identify the device type
-            ref XInputCapabilities pCapabilities  // [out] Receives the capabilities
-        );
+        public static int XInputGetState
+            (
+            int dwUserIndex, // [in] Index of the gamer associated with the device
+            ref XInputState pState // [out] Receives the current state
+            )
+        {
+            return IsWin8OrNewer ? XInput14.XInputGetState(dwUserIndex, ref pState) : XInput910.XInputGetState(dwUserIndex, ref pState);
+        }
+
+        public static int XInputSetState
+            (
+            int dwUserIndex, // [in] Index of the gamer associated with the device
+            ref XInputVibration pVibration // [in, out] The vibration information to send to the controller
+            )
+        {
+            return IsWin8OrNewer ? XInput14.XInputSetState(dwUserIndex, ref pVibration) : XInput910.XInputSetState(dwUserIndex, ref pVibration);
+        }
+
+        public static int XInputGetCapabilities
+            (
+            int dwUserIndex, // [in] Index of the gamer associated with the device
+            int dwFlags, // [in] Input flags that identify the device type
+            ref XInputCapabilities pCapabilities // [out] Receives the capabilities
+            )
+        {
+            return IsWin8OrNewer ? XInput14.XInputGetCapabilities(dwUserIndex, dwFlags, ref pCapabilities) : XInput910.XInputGetCapabilities(dwUserIndex, dwFlags, ref pCapabilities);
+        }
 
 
         //this function is not available prior to Windows 8
         public static int XInputGetBatteryInformation
-        (
-              int dwUserIndex,        // Index of the gamer associated with the device
-              byte devType,            // Which device on this user index
+            (
+            int dwUserIndex, // Index of the gamer associated with the device
+            byte devType, // Which device on this user index
             ref XInputBatteryInformation pBatteryInformation // Contains the level and types of batteries
-        )
+            )
         {
-           return 0;
+            return IsWin8OrNewer ? XInput14.XInputGetBatteryInformation(dwUserIndex, devType, ref pBatteryInformation) : XInput910.XInputGetBatteryInformation(dwUserIndex, devType, ref pBatteryInformation);
         }
 
         //this function is not available prior to Windows 8
         public static int XInputGetKeystroke
-        (
-            int dwUserIndex,              // Index of the gamer associated with the device
-            int dwReserved,               // Reserved for future use
-           ref      XInputKeystroke pKeystroke    // Pointer to an XINPUT_KEYSTROKE structure that receives an input event.
-        )
+            (
+            int dwUserIndex, // Index of the gamer associated with the device
+            int dwReserved, // Reserved for future use
+            ref XInputKeystroke pKeystroke // Pointer to an XINPUT_KEYSTROKE structure that receives an input event.
+            )
         {
-            return 0;
+            return IsWin8OrNewer ? XInput14.XInputGetKeystroke(dwUserIndex, dwReserved, ref pKeystroke) : XInput910.XInputGetKeystroke(dwUserIndex, dwReserved, ref pKeystroke);
         }
-#else
-
-
-        [DllImport("xinput1_4.dll")]
-        public static extern int XInputGetState
-        (
-            int dwUserIndex,  // [in] Index of the gamer associated with the device
-            ref XInputState pState        // [out] Receives the current state
-        );
-
-        [DllImport("xinput1_4.dll")]
-        public static extern int XInputSetState
-        (
-            int dwUserIndex,  // [in] Index of the gamer associated with the device
-            ref XInputVibration pVibration    // [in, out] The vibration information to send to the controller
-        );
-
-        [DllImport("xinput1_4.dll")]
-        public static extern int XInputGetCapabilities
-        (
-            int dwUserIndex,   // [in] Index of the gamer associated with the device
-            int dwFlags,       // [in] Input flags that identify the device type
-            ref XInputCapabilities pCapabilities  // [out] Receives the capabilities
-        );
-
-
-        [DllImport("xinput1_4.dll")]
-        public static extern int XInputGetBatteryInformation
-        (
-              int dwUserIndex,        // Index of the gamer associated with the device
-              byte devType,            // Which device on this user index
-            ref XInputBatteryInformation pBatteryInformation // Contains the level and types of batteries
-        );
-
-        [DllImport("xinput1_4.dll")]
-        public static extern int XInputGetKeystroke
-        (
-            int dwUserIndex,              // Index of the gamer associated with the device
-            int dwReserved,               // Reserved for future use
-           ref      XInputKeystroke pKeystroke    // Pointer to an XINPUT_KEYSTROKE structure that receives an input event.
-        );
-#endif
     }
-
-
 }
