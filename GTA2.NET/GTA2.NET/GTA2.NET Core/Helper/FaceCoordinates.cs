05a2558efd7374b093ee1d0db72d4f07275d3428
joaopires51@gmail.com <joaopires51@gmail.com@c269392e-2877-11df-ab0a-9f3f633ae91d>
2013-11-27 22:25:55 +0000

    Add more functionalities to FaceCoordinates

--- GTA2.NET/GTA2.NET/GTA2.NET Core/Helper/FaceCoordinates.cs
+++ GTA2.NET/GTA2.NET/GTA2.NET Core/Helper/FaceCoordinates.cs
@@ -12,6 +12,7 @@ namespace Hiale.GTA2NET.Core.Helper
         public Vector3 BottomRight;
         public Vector3 TopLeft;
         public Vector3 TopRight;
+        public Vector3 CenterPos { get; set; }
 
         public FaceCoordinates(Vector3 topLeft, Vector3 topRight, Vector3 bottomRight, Vector3 bottomLeft)
         {
@@ -19,6 +20,30 @@ namespace Hiale.GTA2NET.Core.Helper
             TopRight = topRight;
             BottomRight = bottomRight;
             BottomLeft = bottomLeft;
+        }
+
+        public FaceCoordinates(Vector3 centerPos, float width, float height)
+        {
+            TopLeft     = new Vector3(centerPos.X - (width / 2), -(centerPos.Y - (height / 2)), centerPos.Z);
+            TopRight    = new Vector3(centerPos.X + (width / 2), -(centerPos.Y - (height / 2)), centerPos.Z);
+            BottomRight = new Vector3(centerPos.X + (width / 2), -(centerPos.Y + (height / 2)), centerPos.Z);
+            BottomLeft  = new Vector3(centerPos.X - (width / 2), -(centerPos.Y + (height / 2)), centerPos.Z);
+            CenterPos = centerPos;
+        }
+
+        /// <summary>
+        /// Rotates all the vertices around CenterPos by angle radians. 
+        /// </summary>
+        /// <param name="angle">The angle to rotate.</param>
+        public FaceCoordinates Rotate(float angle)
+        {
+            Vector2 rotationPoint = new Vector2(CenterPos.X, -CenterPos.Y);
+            Vector3 tLeft = new Vector3(Geometry.RotatePoint(new Vector2(TopLeft.X, TopLeft.Y), rotationPoint, angle), TopLeft.Z);
+            Vector3 tRight = new Vector3(Geometry.RotatePoint(new Vector2(TopRight.X, TopRight.Y), rotationPoint, angle), TopRight.Z);
+            Vector3 bRight = new Vector3(Geometry.RotatePoint(new Vector2(BottomRight.X, BottomRight.Y), rotationPoint, angle), BottomRight.Z);
+            Vector3 bLeft = new Vector3(Geometry.RotatePoint(new Vector2(BottomLeft.X, BottomLeft.Y), rotationPoint, angle), BottomLeft.Z);
+
+            return new FaceCoordinates(tLeft, tRight, bRight, bLeft);
         }
     }
 }
